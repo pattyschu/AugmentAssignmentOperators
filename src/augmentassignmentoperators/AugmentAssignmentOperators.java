f88b0d7be7d3ab983a313f4c7ce25f86791dff6e
pattyschu <pattyschu@Joshua-Compu>
2015-09-07 12:24:13 -0400

    new commit

---
+++ src/augmentassignmentoperators/AugmentAssignmentOperators.java
@@ -0,0 +1,19 @@
+package practiceJAVAchapter2;
+
+/**
+ *
+ * @author pattyschu
+ */
+public class AugmentAssignmentOperators 
+{
+    public static void main(String[] args)
+    {
+        double a = 6.5;
+        a += a + 1;
+        System.out.println(a);
+        a = 6;
+        a /= 2;
+        System.out.println(a);
+   
+    }
+}
